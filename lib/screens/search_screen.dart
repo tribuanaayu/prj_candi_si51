db27e4ab1bedfc76f02260f876685c184cbefa1a
tribuanaayu <tribuanaayu72@gmail.com>
2024-11-13 17:34:38 +0700

    lanjutan

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _SearchScreen extends StatefulWidget {
+  const _SearchScreen({super.key});
+
+  @override
+  State<_SearchScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<_SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
